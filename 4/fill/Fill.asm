3a6bedf1f9603a014720eced28c33a2c908ba208
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-11-24 16:43:48 +0700

    implment mult and fill hack assembly program

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,65 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+
+@RESET
+0;JMP
+
+(LOOP)
+  // pos = pos - 1
+  @pos
+  M=M-1
+  // if pos < 0 goto RESET
+  @pos
+  D=M
+  @RESET
+  D;JLT
+  // if (key is not pressed) goto WHITE
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+  // goto BLACK 
+  @BLACK
+  0;JMP
+
+
+(WHITE)
+  // color = 0
+  @color
+  M=0
+  // goto PAINT
+  @PAINT
+  0;JMP
+
+(BLACK)
+  // color = 0
+  @color
+  M=-1
+  // goto PAINT
+  @PAINT
+  0;JMP
+
+(PAINT)
+  // set screen to color
+  @color
+  D=M
+  @pos
+  A=M
+  M=D
+  @LOOP
+  0;JMP
+
+(RESET) 
+  // color = 0
+  @color
+  M=0
+  // pos = SCREEN + 8192
+  @SCREEN
+  D=A
+  @8192
+  D=D+A
+  @pos
+  M=D
+  @LOOP
+  0;JMP
--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -7,4 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// i = 1
+@i
+M=1
+// mult = 0
+@mult
+M=0
+(LOOP)
+  // if (i > R1) goto STOP
+  @i
+  D=M
+  @R1
+  D=D-M
+  @STOP
+  D;JGT
+  // mult = mult + R0
+  @mult
+  D=M
+  @R0
+  D=D+M
+  @mult
+  M=D
+  // i = i + 1
+  @i
+  M=M+1
+  // goto LOOP
+  @LOOP
+  0;JMP
+(STOP)
+  // R2 = mult
+  @mult
+  D=M
+  @R2
+  M=D
+(END)
+  @END
+  0;JMP
